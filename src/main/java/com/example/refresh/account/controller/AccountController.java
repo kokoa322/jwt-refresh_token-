41bc2e93a9b7e193192ea1cc539454aac762113a
kokoa322 <tjsgus223@gmail.com>
2023-12-01 03:10:45 +0900

    @AuthenticationPrincipal 적용후 확인

--- src/main/java/com/example/refresh/account/controller/AccountController.java
+++ src/main/java/com/example/refresh/account/controller/AccountController.java
@@ -4,8 +4,10 @@ import com.example.refresh.account.dto.AccountReqDto;
 import com.example.refresh.account.dto.LoginReqDto;
 import com.example.refresh.account.service.AccountService;
 import com.example.refresh.global.dto.GlobalResDto;
+import com.example.refresh.security.user.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,5 +39,12 @@ public class AccountController {
     public void logout (@RequestBody LoginReqDto loginReqDto){
         log.info("loginReqDto.toString() --> : {}", loginReqDto.toString());
         accountService.logout(loginReqDto);
+    }
+
+    @PostMapping("/test1")
+    public void test1 (@RequestBody LoginReqDto loginReqDto, @AuthenticationPrincipal
+    UserDetailsImpl userDetails){
+        log.info("/test1 --> : loginReqDto.toString() --> : {}", loginReqDto.toString());
+        log.info("/test1 --> : userDetails.getUsername() --> : {}", userDetails.getUsername());
     }
 }
--- src/main/java/com/example/refresh/account/service/LogoutService.java
+++ src/main/java/com/example/refresh/account/service/LogoutService.java
@@ -1,13 +1,9 @@
 package com.example.refresh.account.service;
 
-import com.example.refresh.account.dto.LoginReqDto;
-import com.example.refresh.account.entity.Account;
-import com.example.refresh.account.repository.AccountRepository;
 import com.example.refresh.account.repository.RefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
 
